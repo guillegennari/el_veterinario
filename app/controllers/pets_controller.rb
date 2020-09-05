8c097e3debda3324c12427ecbf747fe622a3751d
Guillermo <mitogennari@gmail.com>
2020-09-05 17:35:13 -0400

    plataforma veterinario con pets anidados en clients

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,10 +1,12 @@
 class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
+  before_action :current_client
 
   # GET /pets
   # GET /pets.json
   def index
-    @pets = Pet.all
+    
+    @pets = @client.pets
   end
 
   # GET /pets/1
@@ -15,10 +17,12 @@ class PetsController < ApplicationController
   # GET /pets/new
   def new
     @pet = Pet.new
+    @clients = Client.pluck :name, :id
   end
 
   # GET /pets/1/edit
   def edit
+    @clients = Client.pluck :name, :id
   end
 
   # POST /pets
@@ -69,6 +73,11 @@ class PetsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pet_params
-      params.require(:pet).permit(:name, :race, :birthdate)
+      params.require(:pet).permit(:name, :race, :birthdate, :client_id)
     end
+
+    def current_client
+      @client = Client.find(params[:client_id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  #resources :clients
   resources :pet_histories
-  resources :pets
+
+  resources :clients do
+    resources :pets
+  end
+
+  #resources :pets
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pets#index"
 end
